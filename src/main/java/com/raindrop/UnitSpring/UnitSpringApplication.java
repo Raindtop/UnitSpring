5de84829d1a928c93ff8e1ba8b8073babd23e018
Raindtop <56855282+Raindtop@users.noreply.github.com>
2019-10-22 11:21:33 +0800

    Add files via upload

---
+++ src/main/java/com/raindrop/UnitSpring/UnitSpringApplication.java
@@ -0,0 +1,13 @@
+package com.raindrop.UnitSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raindrop/UnitSpring/UnitSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.raindrop.UnitSpring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UnitSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
